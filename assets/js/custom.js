48f840445cb8e6ca90b21c2770f21591b0611986
Heelie <Heelie.hj@gmail.com>
2022-12-14 21:00:29 +0800

    up: custom.js & favorites.scss

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
+const _hmt = _hmt || [];
+(function () {
+    const hm = document.createElement("script");
     hm.src = "https://hm.baidu.com/hm.js?043d3768d4c10b2446819a6939aaf7e5";
-    var s = document.getElementsByTagName("script")[0];
+    const s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
